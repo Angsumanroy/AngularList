b0efd527b106490c733d48e676309e5ad2020793
Angsuman Roy <angsuman.roy@live.com>
2019-11-16 08:09:58 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using MVCTraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTraining.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details(int id)
+        {
+            EmployeeContext empCon = new EmployeeContext();
+            Employee employee = empCon.Employees.Single(emp => emp.EmployeeID == id);
+            return View(employee);
+        }
+    }
+}
